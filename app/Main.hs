172827b540869c47e21d88d30bdb9e61d8adb395
proc0 <alanvx@gmail.com>
2019-12-11 00:03:15 -0800

    fixed main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
-import Lib
+import Data.Either 
+import Text.Pretty.Simple (pPrint)
+import EitherJson (Parser, decode, json, Result)
+import Alias
+
+parseFile :: 
+    FilePath 
+ -> Parser json 
+ -> IO (Result json)
+parseFile path parser = do
+    file <- readFile path
+    let result = decode parser file
+    return result
+
+readJson :: FilePath -> IO ()
+readJson path = do
+    output <- parseFile path json
+    -- return $ do
+    --     pPrint <$> output
+    case output of
+        Right (source, result) -> pPrint result
+        Left stack -> pPrint stack
+        
+filepath = "./src/tests/mock/valid2.json"
 
 main :: IO ()
-main = someFunc
+main = do
+    readJson filepath
